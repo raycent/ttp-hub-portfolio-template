77c17dd2d3773d07007fbc664d7def34ad39d36e
Raycent <raycent.tan@flatironschool.com>
2017-02-17 19:39:16 -0500

    json format

---
+++ assets/data.js
@@ -0,0 +1,68 @@
+{
+  "image": "",
+  "name": "Fidel Severino",
+  "title": "Web Developer",
+  "summary": "Outstanding professional with metaphysical insight and two cats.",
+  "location": "New York City",
+  "email": "fidel.severino@flatironschool.com",
+  "socialMedia": {
+    "linkedin": "",
+    "github": "",
+    "slack": "",
+    "twitter": "",
+    "facebook": ""
+  },
+  "education": [
+    {
+      "date": "2012-2014",
+      "degree": "Master Degree",
+      "name": "Computer University",
+      "details": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    },
+    {
+      "date": "2010-2012",
+      "degree": "Bachelor Degree",
+      "name": "Design University",
+      "details": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    }
+  ],
+  "skills": [
+    "Ruby on Rails", "JavaScript", "React", "Redux", "SQL", "MySQL", "Python"
+  ],
+  "experience": [
+    {
+      "date": "2013-2015",
+      "companyName": "ThemeForest",
+      "positionTitle": "Front-End Developer",
+      "details": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    },
+    {
+      "date": "2011-2013",
+      "companyName": "PhotoDune",
+      "positionTitle": "Photographer",
+      "details": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    },
+    {
+      "date": "2009-2011",
+      "companyName": "GraphicRiver",
+      "positionTitle": "Graphic Designer",
+      "details": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    }
+  ],
+  "projects": [
+    {
+      "projectTitle": "Something",
+      "skills": [
+        "Ruby on Rails", "APIs", "jQuery"
+      ],
+      "projectLink": ""
+    },
+    {
+      "projectTitle": "Something else",
+      "skills": [
+        "JavaScript", "React", "Redux"
+      ],
+      "projectLink": ""
+    }
+  ]
+}
